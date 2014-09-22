c7390db0aabf1a7620e3f64c3b6228903c469da1
Ed Kimber <edward.kimber@gmail.com>
2014-09-23 00:03:10 +0100

    Bounding box calculation

--- project.clj
+++ project.clj
@@ -11,23 +11,13 @@
                  [org.clojure/tools.cli "0.2.2"]
                  [compojure "1.1.5"]
                  [ring/ring-core "1.1.8"]
-
                  [org.clojure/data.json "0.2.1"]
-
                  [http-kit "2.1.16"]
-
-                 [http-kit/dbcp "0.1.0"] ;; database access
-
-                 [mysql/mysql-connector-java "5.1.21"] ;; mysql jdbc driver
-
                  ;; [org.fressian/fressian "0.6.3"]
-
                  ;; for serialization clojure object to bytes
                  ;; [com.taoensso/nippy "1.1.0"]
-
                  ;; Redis client & message queue
                  ;; [com.taoensso/carmine "1.5.0"]
-
                  ;; logging,  another option [com.taoensso/timbre "1.5.2"]
                  [org.clojure/tools.logging "0.2.6"]
                  [ch.qos.logback/logback-classic "1.0.1"]
--- test/clj_strava/easy_tests.clj
+++ test/clj_strava/easy_tests.clj
@@ -1,1 +1,16 @@
+(ns clj-strava.easy-tests
+  (:require [clj-strava.polyline :as poly]
+            [clojure.test :refer :all]))
+
 (poly/decode "y`tsHnhv[gI|FoAfF`@nC~IhBtPgAvDzBjGdb@R`U~AbI~M~D|WF`QcGzIBnh@pIpR`[jd@zUnHpOt]jIxd@bYvt@|GsKba@qAvQuJve@aHbNiIdBwEnFMci@qKeaAwAq[{VmN}\\kIgHgO_OkKgUcJmRwZ}i@oIaIDmOzFeYOqMiE{AeIW}ToGka@{DgC{PhAuIeCGeDtBeGpEwD")
+
+(def points [{:longitude 0.5 :latitude 1}
+             {:longitude -0.5 :latitude 0.5}
+             {:longitude 1.2 :latitude 0.1}])
+
+(deftest finding-bounding-box
+         (is 
+           (= [[0.1 -0.5]
+               [0.1 1.2]
+               [1.2 -0.5]
+               [1.2 1.2]])))
