c492493dc75346e00a6b75d0eb3032aef8eb44d0
ekimber <edward.kimber@gmail.com>
2014-09-27 12:39:23 +0100

    Added more strava API endpoints,

--- src/clj_strava/strava.clj
+++ src/clj_strava/strava.clj
@@ -3,7 +3,6 @@
             [org.httpkit.client :as http]
             [clojure.tools.logging :as log]
             [clojure.data.json :as json]
-            [clj-strava.polyline :as poly]
             [clj-strava.client :as client])
   (:import [java.net URI URLEncoder])
   (:use     [clojure.core.async :as async :only [chan go >! <!!]]))
@@ -30,7 +29,9 @@
 (defn exchange-tokens [code]
    {:access-token ((access-token code) "access_token")})
 
-(defn replace-keywords [url keymap]
+(defn replace-keywords
+  "Replace url params like so: /v3/activities/:id {:id 123} -> /v3/activities/123"
+  [url keymap]
   (if (empty? keymap)
     url
     (let [k (first (keys keymap))]
@@ -42,8 +43,7 @@
   ([url]
    (str endpoint url))
   ([url params]
-   ()))
-
+   (url-builder (replace-keywords url params))))
 
 ;TODO could simplify this macro
 (defmacro defapifn
@@ -58,14 +58,29 @@
              (url-builder ~url params#)
              (auth-header token#))))))
 
+#_(defmacro tm [arg & ps]
+  `(defn a ~(if ps '[arg f] '[arg]) f))
+
+#_(macroexpand '(tm 1))
+
 (defapifn activities "/v3/athlete/activities")
+(defapifn activity "/v3/activities/:id" :id)
+(defapifn activity-streams "/v3/activities/:id/streams" :id)
 
-(defn activity [token activity-id]
-  (<!! (client/json-get
-         (str endpoint "/v3/activities/" activity-id)
-         (auth-header token))))
+(defapifn athlete "/v3/athlete")
+(defapifn athletes "/v3/athletes/:id" :id)
+(defapifn friends "/v3/athlete/friends")
+(defapifn athlete-friends "/v3/athletes/:id/friends" :id)
+(defapifn followers "/v3/athlete/followers")
+(defapifn athlete-followers "/v3/athletes/:id/followers" :id)
+(defapifn both-following "/v3/athletes/:id/both-following" :id)
 
-(defn athlete [token athlete-id ]
-  (<!! (client/json-get
-         (str endpoint "/v3/athletes/" athlete-id)
-         (auth-header token))))
+(defapifn clubs "/v3/clubs/:id" :id)
+(defapifn gear "/v3/gear/:id" :id)
+
+(defapifn segments "/v3/segments/:id" :id)
+(defapifn leaderboard "/v3/segments/:id/leaderboard" :id)
+(defapifn all-efforts "/v3/segments/:id/all_efforts" :id)
+
+(defapifn uploads "/v3/uploads")
+
